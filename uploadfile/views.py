5d81c550b5073fdaceca5dc82571818f97bbbfd9
Nihal Londhe <nihallondhe20@gmail.com>
2023-09-27 08:35:14 +0530

    Add particular with search field

--- uploadfile/views.py
+++ uploadfile/views.py
@@ -8,51 +8,124 @@ import pandas as pd
 from django.contrib import messages
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
-
+import pandas as pd
+from django.http import HttpResponse
+from io import BytesIO
+import openpyxl
 @login_required(login_url='/login')
 @login_required
 def uploadcsv(request):
     if request.method == 'POST':
-        
+        form = storeform(request.POST, request.FILES)
+        excel_file = request.FILES['file']
+        df = pd.read_excel(excel_file)
+        if not excel_file.name.endswith('.xlsx'):
+            return HttpResponse('Invalid file format. Please upload an XLSX file.')
+
         try:
-            form = storeform(request.POST, request.FILES)
-            excel_file = request.FILES['file']
-            data = pd.read_excel(excel_file)
-            
-            print('working')
-            for index, row in data.iterrows():
-                # date = str(row['Date']).strip()
-                # total_amount =str(row['Particulars'])
-                # datas = {}
-                # if date :
-                #     datass = {date:[]}
-                #     if date == 'NaT':
-                #         datass =  date[total_amount]
-                #     print(datass)
-                   
-                # datas = datass
-                
-                    
-                date = str(row['Date']).strip()
-                buyer = str(row['Consignee/Buyer']).strip()
-                address = str(row['Address'])
-                total_amount =str(row['Gross Total'])
-                
-                
-                if (date != "NaT"):
-                    user = Buyerdata(date=date, buyer=buyer, address=address,total_amount=total_amount)
-                    user.full_clean() # Validate fields
-                    user.save()
-                    
-            if form.is_valid():
-                form.save()
-                return redirect('/home')  # Redirect to a success page or any other page
+            df = pd.read_excel(excel_file, engine='openpyxl', header=None)
         except Exception as e:
-            messages.error(request, 'Unable to upload file. ' + repr(e))
+            return HttpResponse(f'Error reading Excel file: {str(e)}')
+
+        transactions = []
+
+        current_date = None
+        current_particulars = []
+        print(df,'this is data')
+        for index, row in df.iterrows():
+            date = row[0]
+            buyer = row[2]
+            particulars = row[1]    
+            address = row[3]
+
+            if pd.notna(date):
+                # If date changes, save the previous data and start a new group
+                if current_date is not None:
+                    transaction = Buyerdata(
+                        date=current_date,
+                        buyer=buyer,
+                        total_amount=" ".join(current_particulars),
+                        address=address
+                    )
+                    transactions.append(transaction)
+                    current_particulars = []
+
+                current_date = date
+
+            if pd.notna(particulars):
+                current_particulars.append(particulars)
+                
+        Buyerdata.objects.bulk_create(transactions)
+        print(current_particulars)
+
+        
+                # excel_data.save()
     else:
         form = storeform()
-    return render(request, 'fileupload.html', {'form': form})
+        return render(request, 'fileupload.html', {'form': form})
+
+
+
+# def uploadcsv(request):
+#     if request.method == 'POST':
+        
+#         try:
+#             form = storeform(request.POST, request.FILES)
+#             excel_file = request.FILES['file']
+#             data = pd.read_excel(excel_file)
+            
+#             c= 0
+           
+#             p=[]
+#             for indexx, row in data.iterrows():
+#                 if isinstance(row[0],pd.Timestamp):
+#                     # p.append(index,row[0])
+#                      p.append(indexx)
+            
+               
+#             print(p)
+           
+
+
+
+
+
+            
+              
+                
+               
+                # i = index
+                # # date = str(row['Particulars']).strip()
+                # buyer = str(row['Consignee/Buyer']).strip()
+                # address = str(row['Address'])
+                # total_amount =str(row['Gross Total'])
+                # print(row[0])
+               
+                    
+                
+                
+                
+                    
+                    
+                        
+                        
+                   
+                
+                # if (date != "NaT"):
+                #     user = Buyerdata(date=date, buyer=buyer, address=address,total_amount=total_amount)
+                #     user.full_clean() # Validate fields
+                #     user.save()
+                    
+    #         if form.is_valid():
+    #             form.save()
+    #             return redirect('/home')  # Redirect to a success page or any other page
+    #     except Exception as e:
+    #         messages.error(request, 'Unable to upload file. ' + repr(e))
+    # else:
+    #     form = storeform()
+    # return render(request, 'fileupload.html', {'form': form})
 
 
 @login_required(login_url='/loginn')
@@ -64,33 +137,33 @@ def home(request):
     
         # Search functionality
         search_query = request.GET.get('search')
-        uname = request.GET.get('name')
+        uname = request.GET.get('total')
         address = request.GET.get('address')
-        total = request.GET.get('Total')
-        ID = request.GET.get('ID')
-        datee = request.GET.get('date')
+        # total = request.GET.get('Total')
+        # ID = request.GET.get('ID')
+        # datee = request.GET.get('date')
         if search_query:
             users = users.filter(address__icontains=address)
             context = {'users': users}
             return render(request, 'home.html', context)
-        elif search_query:
-            users = users.filter(address__icontains=address)
-            context = {'users': users}
-            return render(request, 'home.html', context)
-        elif total:
-            users = users.filter(total_amount__icontains=total)
-            context = {'users': users}
-            return render(request, 'list.html', context)
-        elif ID:
-            users = users.filter(id__icontains=ID)
-            context = {'users': users}
-            return render(request, 'home.html', context)
-        elif datee:
-            users = users.filter(date__icontains=datee)
-            context = {'users': users}
-            return render(request, 'home.html', context)
+        # elif search_query:
+        #     users = users.filter(address__icontains=address)
+        #     context = {'users': users}
+        #     return render(request, 'home.html', context)
+        # elif total:
+        #     users = users.filter(total_amount__icontains=total)
+        #     context = {'users': users}
+        #     return render(request, 'list.html', context)
+        # elif ID:
+        #     users = users.filter(id__icontains=ID)
+        #     context = {'users': users}
+        #     return render(request, 'home.html', context)
+        # elif datee:
+        #     users = users.filter(date__icontains=datee)
+        #     context = {'users': users}
+        #     return render(request, 'home.html', context)
         elif uname:
-            users = users.filter(buyer__icontains=uname)
+            users = users.filter(total_amount__icontains=uname)
             context = {'users': users}
             return render(request, 'list.html', context)
         
